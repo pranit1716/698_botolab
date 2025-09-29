8827c1ee48f3bceaea5b8b83f63740dd59e5efce
Pranit Pramod Patil <87650134+pranit1716@users.noreply.github.com>
2025-09-29 18:13:06 -0400

    Add files via upload

---
+++ create_bucket.py
@@ -0,0 +1,6 @@
+import boto3
+
+s3 = boto3.client('s3')
+bucket_name = 'my-boto3-s3-bucket-botoppp' 
+response = s3.create_bucket(Bucket=bucket_name)
+print(f'Bucket {bucket_name} created successfully!')
